6108a06a5cee596017cf755120fcc2f0d1829371
Cristian <piguazu@gmail.com>
2017-09-03 17:05:16 -0300

    Modificacion de cuadros persona/entidad Ok botón seleccionar persona en requisitos para alta de registro

--- metadatos/componentes/toba_ci/dump_1000891.sql
+++ metadatos/componentes/toba_ci/dump_1000891.sql
@@ -214,6 +214,18 @@ INSERT INTO apex_objeto_dependencias (proyecto, dep_id, objeto_consumidor, objet
 	NULL, --inicializar
 	NULL  --orden
 );
+INSERT INTO apex_objeto_dependencias (proyecto, dep_id, objeto_consumidor, objeto_proveedor, identificador, parametros_a, parametros_b, parametros_c, inicializar, orden) VALUES (
+	'sgr', --proyecto
+	'1000689', --dep_id
+	'1000891', --objeto_consumidor
+	'1000900', --objeto_proveedor
+	'form_ml_reg', --identificador
+	NULL, --parametros_a
+	NULL, --parametros_b
+	NULL, --parametros_c
+	NULL, --inicializar
+	NULL  --orden
+);
 --- FIN Grupo de desarrollo 1
 
 ------------------------------------------------------------
@@ -284,6 +296,13 @@ INSERT INTO apex_objetos_pantalla (proyecto, pantalla, objeto_ci, orden, dep_id)
 	'1000891', --objeto_ci
 	'0', --orden
 	'1000684'  --dep_id
+);
+INSERT INTO apex_objetos_pantalla (proyecto, pantalla, objeto_ci, orden, dep_id) VALUES (
+	'sgr', --proyecto
+	'1000465', --pantalla
+	'1000891', --objeto_ci
+	'3', --orden
+	'1000689'  --dep_id
 );
 
 ------------------------------------------------------------
