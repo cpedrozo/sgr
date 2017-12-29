b699c2251435349833eb47bb7d32e9cb545087da
Cristian <piguazu@gmail.com>
2017-12-29 11:24:29 -0300

    Ok 1 Filtro autocargado funciona solo cuando creo un nuevo registro 2 Finalizar registro (evt__procesar2) desde su creacion

--- metadatos/componentes/toba_item/dump_1000318.sql
+++ metadatos/componentes/toba_item/dump_1000318.sql
@@ -1,5 +1,5 @@
 ------------------------------------------------------------
---[1000318]--  Datos Personas 
+--[1000318]--  Detalles por persona 
 ------------------------------------------------------------
 
 ------------------------------------------------------------
@@ -23,7 +23,7 @@ INSERT INTO apex_item (item_id, proyecto, item, padre_id, padre_proyecto, padre,
 	NULL, --actividad_buffer
 	NULL, --actividad_patron_proyecto
 	NULL, --actividad_patron
-	'Datos Personas', --nombre
+	'Detalles por persona', --nombre
 	NULL, --descripcion
 	'1000001', --punto_montaje
 	NULL, --actividad_accion
