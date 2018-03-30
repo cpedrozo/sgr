b2029834d15e1fbfe8469d2cc0d67ef65abfbbfa
Cristian <piguazu@gmail.com>
2018-03-30 08:57:15 -0300

    Se comprobo que funciona el metodo y script para el bkp de db.

--- php/operaciones/ayuda/bkp/ci_bkp.php
+++ php/operaciones/ayuda/bkp/ci_bkp.php
@@ -3,26 +3,44 @@ class ci_bkp extends sgr_ci
 {
 	protected $s__datos = [];
 	protected $scriptCrearBackup = "/home/toba_2_6_7/toba_2_7_6/proyectos/sgr/bin/script_bkp_db";
-
+	protected $modoDesarrollador = false;
+	
 	function ejecutarComando($comando)
 	{
 		$output = [];
 		$resultado = -1;
 		exec("$comando 2>&1", $output, $resultado);
-		$this->s__datos['backup-log'] =
-			['output' => $output,
-			 'resultado' => $resultado,
-			 'descargar' => '<a href="temp/backup.backup.gz">Descargar Backup</a>'];
+		// Valor de retorno de la función
+		$unResultado = ['resultado' => $resultado,
+			 							'output' => $output];
+		// Usamos s__datos['backup-log'] en la función esta_backupCreado para
+		// determinar si el backup fue exitoso o no
+		$this->s__datos['backup-log'] = $unResultado;
+		// == DEBUG ==
+		// Usamos esta variable para imprimir información de debug
+		$unaInformacionLog = array_merge(['comando' => $comando],
+	 											             $unResultado);
+		// Si la orden se ejecutó correctamente
+		if ($resultado == 0) {
+			// Imprimimos la información de debug
+			toba::logger()->debug('Se ejecutó la orden: '.$unaInformacionLog['comando']);
+		} else {
+			// De lo contrario imprimimos la información de debug informando de un error
+			toba::logger()->error('Falló la orden: '.$unaInformacionLog['comando']);
+		}
+		// Imprimimos todo lo guardado en $unaInformacionLog
+		toba::logger()->var_dump($unaInformacionLog, 'Ejecucion de comando');
+		// == FIN DEBUG ==
+		return $unResultado;
 	}
 	function crearBackup()
 	{
-		$resultado = 0;
 		// Obtenemos la clave
-		$this->ejecutarComando($this->scriptCrearBackup. " get-pass");
-		if ($resultado==0) {
-			if (isset($output[0])) {
+		$unResultado = $this->ejecutarComando($this->scriptCrearBackup. " get-pass");
+		if ($unResultado['resultado']==0) {
+			if (isset($unResultado['output'][0])) {
 				// Seteamos la clave
-				putenv("PGPASSWORD=" . $output[0]);
+				putenv("PGPASSWORD=" . $unResultado['output'][0]);
 			}
 		}
 		// Creamos el backup
